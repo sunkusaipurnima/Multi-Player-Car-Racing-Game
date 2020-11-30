064ba26b9604575d5dae1f962eede96155e5d2d7
Sunku Sai Purnima <69891257+sunkusaipurnima@users.noreply.github.com>
2020-11-30 10:58:22 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,52 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+    this.reset = createButton('Reset');
+    this.winner= createElement('h2');
+  }
+  hideFields(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 10, displayHeight/2);
+    this.reset.position(displayWidth/2+70,displayHeight/2);
+2
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+
+    this.reset.mousePressed(()=>{
+    
+      player.updateCount(0);
+      Player.updateCarsAtEnd(0);
+      game.update(0);
+
+
+    });
+ }
+
+  displayWinners(playerName,playerPosition){
+     this.winner.html("Congratulations"+playerName+","+ "you are in position :"+playerPosition);
+     this.winner.position(displayWidth/2 - 100, displayHeight/4-30);
+ }
+}
